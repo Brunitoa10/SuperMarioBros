e4e38eea2fafc7a961acd68db2f35b267f669255
Bruno A Parisi <bnoprs00@gmail.com>
2024-10-11 16:36:12 -0300

    Muestro pantalla principal

--- src/Launcher/Launcher.java
+++ src/Launcher/Launcher.java
@@ -4,7 +4,6 @@ import java.awt.EventQueue;
 
 import Logica.Juego;
 import Vista.GUI;
-import Vista.Controladores.ControladorVista;
 
 public class Launcher {
     public static void main(String[] args) {
@@ -12,7 +11,7 @@ public class Launcher {
             public void run() {
                 try {
                     Juego juego = new Juego();
-                    ControladorVista controlador_vistas = new GUI(juego);
+                    GUI controlador_vistas = new GUI(juego);
                     juego.set_controlador_vistas(controlador_vistas);
                     controlador_vistas.mostrar_pantalla_inicial();
                 } catch (Exception e) {
