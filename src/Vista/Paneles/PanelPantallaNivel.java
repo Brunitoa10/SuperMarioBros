3c8f5b846a129b00fa458167a38f1dca26c02a9a
Bruno A Parisi <bnoprs00@gmail.com>
2024-10-12 16:29:59 -0300

    Acomode visulamente puntaje vida y tiempo

--- src/Vista/Paneles/PanelPantallaNivel.java
+++ src/Vista/Paneles/PanelPantallaNivel.java
@@ -21,12 +21,10 @@ public class PanelPantallaNivel extends JPanel {
      */
     private static final long serialVersionUID = 1L;
     private JLabel imagen_fondo_panel_nivel;
-    private JLabel label_puntaje;
-    private JLabel lbl_vida;
-    private JLabel label_vida;
     private JLabel lbl_puntaje_txt;
     private JLabel lbl_Vida_txt;
     private JLabel lbl_tiempo_txt;
+    private JLabel lbl_tiempo;
 
     public PanelPantallaNivel() {
         setPreferredSize(new Dimension(ConstantesVista.PANEL_ANCHO, ConstantesVista.PANEL_ALTO));
@@ -53,9 +51,9 @@ public class PanelPantallaNivel extends JPanel {
     }
 
     protected void actualizar_labels_informacion(EntidadJugador jugador) {
-        label_puntaje.setText(texto_con_cantidad_digitos(jugador.get_puntaje(), 5));
-        lbl_vida.setText(texto_con_cantidad_digitos(jugador.get_tiempo(), 5));
-        label_vida.setText(texto_con_cantidad_digitos(jugador.get_vida(), 5)); // Pedir a nivel?
+    	lbl_puntaje_txt.setText(texto_con_cantidad_digitos(jugador.get_puntaje(), 5));
+    	lbl_Vida_txt.setText(texto_con_cantidad_digitos(jugador.get_tiempo(), 5));
+    	lbl_tiempo_txt.setText(texto_con_cantidad_digitos(jugador.get_vida(), 5)); // Pedir a nivel?
     }
 
     protected String texto_con_cantidad_digitos(int numero, int digitos) {
@@ -95,32 +93,42 @@ public class PanelPantallaNivel extends JPanel {
         // agregar_imagen_fondo_panel_informacion();
 
         setLayout(null);
-        label_puntaje = new JLabel("00000");
-        label_puntaje.setHorizontalAlignment(SwingConstants.CENTER);
-        label_puntaje.setBounds(115, 11, 130, 22);
-        add(label_puntaje);
 
         lbl_puntaje_txt = new JLabel("Puntaje");
+        lbl_puntaje_txt.setFont(new Font("Tahoma", Font.BOLD, 11));
         lbl_puntaje_txt.setHorizontalAlignment(SwingConstants.CENTER);
         lbl_puntaje_txt.setBounds(10, 11, 95, 22);
         add(lbl_puntaje_txt);
 
         lbl_Vida_txt = new JLabel("Vida");
+        lbl_Vida_txt.setFont(new Font("Tahoma", Font.BOLD, 11));
         lbl_Vida_txt.setHorizontalAlignment(SwingConstants.CENTER);
         lbl_Vida_txt.setBounds(285, 11, 95, 22);
         add(lbl_Vida_txt);
-        lbl_vida = new JLabel("0");
-        lbl_vida.setHorizontalAlignment(SwingConstants.CENTER);
-        lbl_vida.setBounds(390, 11, 95, 22);
-        add(lbl_vida);
-        label_vida = new JLabel("00000");
-        label_vida.setBounds(653, 11, 113, 34);
-        add(label_vida);
 
-        lbl_tiempo_txt = new JLabel("tiempo");
+        lbl_tiempo_txt = new JLabel("Tiempo");
+        lbl_tiempo_txt.setFont(new Font("Tahoma", Font.BOLD, 11));
         lbl_tiempo_txt.setHorizontalAlignment(SwingConstants.CENTER);
-        lbl_tiempo_txt.setBounds(537, 17, 95, 22);
+        lbl_tiempo_txt.setBounds(532, 11, 95, 22);
         add(lbl_tiempo_txt);
+        
+        JLabel lbl_puntaje = new JLabel("00000");
+        lbl_puntaje.setHorizontalAlignment(SwingConstants.CENTER);
+        lbl_puntaje.setFont(new Font("Tahoma", Font.PLAIN, 11));
+        lbl_puntaje.setBounds(97, 15, 150, 14);
+        add(lbl_puntaje);
+        
+        JLabel lbl_vida = new JLabel("0");
+        lbl_vida.setHorizontalAlignment(SwingConstants.CENTER);
+        lbl_vida.setFont(new Font("Tahoma", Font.PLAIN, 11));
+        lbl_vida.setBounds(354, 15, 150, 14);
+        add(lbl_vida);
+        
+        lbl_tiempo = new JLabel("00000");
+        lbl_tiempo.setHorizontalAlignment(SwingConstants.CENTER);
+        lbl_tiempo.setFont(new Font("Tahoma", Font.PLAIN, 11));
+        lbl_tiempo.setBounds(607, 15, 150, 14);
+        add(lbl_tiempo);
         agregar_labels_editables_informacion();
     }
 
@@ -141,16 +149,5 @@ public class PanelPantallaNivel extends JPanel {
     }
 
     protected void decorar_labels_informacion() {
-        label_puntaje.setBounds(49, 120, 150, 50);
-        lbl_vida.setBounds(54, 290, 150, 50);
-        label_vida.setBounds(52, 450, 150, 50);
-
-        label_puntaje.setForeground(Color.WHITE);
-        lbl_vida.setForeground(Color.WHITE);
-        label_vida.setForeground(Color.WHITE);
-
-        label_puntaje.setFont(new Font(label_puntaje.getFont().getName(), Font.BOLD, 24));
-        lbl_vida.setFont(new Font(label_puntaje.getFont().getName(), Font.BOLD, 24));
-        label_vida.setFont(new Font(label_puntaje.getFont().getName(), Font.BOLD, 24));
     }
 }
