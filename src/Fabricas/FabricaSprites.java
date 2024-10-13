abb75e4ac67eccc51541741c1f53d9cdacb740a7
francomp258 <francomp258@gmail.com>
2024-10-13 20:31:36 -0300

    Rutas a sprites en FabricaSprites

--- src/Fabricas/FabricaSprites.java
+++ src/Fabricas/FabricaSprites.java
@@ -13,40 +13,40 @@ public abstract class FabricaSprites {
     //TODO: Setear valores de ancho y alto en pixeles para cada entidad, puesto en 16x16 por default
 
     public Sprite crearSpriteGoomba() {
-        return new Sprite(ruta_carpeta + "/", 16, 16);
+        return new Sprite(ruta_carpeta + "/Enemigos/Goomba/Goomba_caminando_loop.gif", 16, 16);
     }
     public Sprite crearSpriteBuzzyBeetle() {
-        return new Sprite(ruta_carpeta + "/", 16, 16);
+        return new Sprite(ruta_carpeta + "/Enemigos/BuzzyBeetle/BuzzyBeetleLeftLoop.gif", 16, 16);
     }
     public Sprite crearSpriteKoopaTroopa(){
-        return new Sprite(ruta_carpeta + "/", 16, 16);
+        return new Sprite(ruta_carpeta + "/Enemigos/KoopaTroopa/KoopaCaminandoLeft.gif", 16, 16);
     }
     public Sprite crearSpriteLakitu() {
-        return new Sprite(ruta_carpeta + "/", 16, 16);
+        return new Sprite(ruta_carpeta + "/Enemigos/Lakitu_moviendose.png", 16, 16);
     }
     public Sprite crearSpriteSpiny() {
-        return new Sprite(ruta_carpeta + "/", 16, 16);
+        return new Sprite(ruta_carpeta + "/Enemigos/Spiny/SpinnyCaminandoLeftLoop.gif", 16, 16);
     }
     public Sprite crearSpriteBowser() {
-        return new Sprite(ruta_carpeta + "/", 16, 16);
+        return new Sprite(ruta_carpeta + "/Enemigos/Bowser/Bowser.png", 16, 16);
     }
     public Sprite crearSpritePiranhaPlant() {
-        return new Sprite(ruta_carpeta + "/", 16, 16);
+        return new Sprite(ruta_carpeta + "/Enemigos/PiranhaPlant/PlantaLoop.gif", 16, 16);
     }
     public Sprite crearSpriteMoneda() {
-        return new Sprite(ruta_carpeta + "/", 16, 16);
+        return new Sprite(ruta_carpeta + "/coin.png", 16, 16);
     }
     public Sprite crearSpriteChampinionVerde() {
-        return new Sprite(ruta_carpeta + "/", 16, 16);
+        return new Sprite(ruta_carpeta + "/life_mushroom.png", 16, 16);
     }
     public Sprite crearSpriteEstrella() {
-        return new Sprite(ruta_carpeta + "/", 16, 16);
+        return new Sprite(ruta_carpeta + "/superstar.png", 16, 16);
     }
     public Sprite crearSpriteFlorDeFuego() {
-        return new Sprite(ruta_carpeta + "/", 16, 16);
+        return new Sprite(ruta_carpeta + "/fire_flower.png", 16, 16);
     }
     public Sprite crearSpriteSuperChampinion() {
-        return new Sprite(ruta_carpeta + "/", 16, 16);
+        return new Sprite(ruta_carpeta + "/mushroom.png", 16, 16);
     }
     public Sprite crearSpriteBolaDeFuego() {
         return new Sprite(ruta_carpeta + "/", 16, 16);
@@ -67,7 +67,7 @@ public abstract class FabricaSprites {
         return new Sprite(ruta_carpeta + "/", 16, 16);
     }
     public Sprite crearSpriteJugador() {
-        return new Sprite(ruta_carpeta + "/", 16, 16);
+        return new Sprite(ruta_carpeta + "/Jugador/PNGMario/StandingMarioLeft.png", 16, 16);
     }
     public Sprite crearSpriteBandera() {
         return new Sprite(ruta_carpeta + "/", 16, 16);
