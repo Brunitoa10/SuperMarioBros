743ecf8e83868fa55872cae03a48c55bcd8aecdc
franpiccio174 <franpiccio174@gmail.com>
2024-10-16 12:21:54 -0300

    Paso a UpperCamelCase

--- src/Vista/ObserverGrafica/ObserverEntidad.java
+++ src/Vista/ObserverGrafica/ObserverEntidad.java
@@ -4,8 +4,8 @@ import Entidades.EntidadLogica;
 
 public class ObserverEntidad extends ObserverGrafica {
 
-    public ObserverEntidad(EntidadLogica entidad_observada) {
-        super(entidad_observada);
+    public ObserverEntidad(EntidadLogica entidadObservada) {
+        super(entidadObservada);
         actualizarObserver();
     }
 }
