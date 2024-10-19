45f88442df248ea3a0b8a2e219c1c061c39ad3bb
francomp258 <francomp258@gmail.com>
2024-10-18 21:22:56 -0300

    Cambios en tamaños de FabricaSprites

--- src/Fabricas/FabricaSprites.java
+++ src/Fabricas/FabricaSprites.java
@@ -8,43 +8,41 @@ public abstract class FabricaSprites {
         this.ruta_carpeta = ruta_carpeta;
     }
 
-    //TODO: Setear valores de ancho y alto en pixeles para cada entidad, puesto en 16x16 por default
-
     public Sprite crearSpriteGoomba() {
-        return new Sprite(ruta_carpeta + "/Enemigos/Goomba/Goomba_caminando_loop.gif", 16, 16);
+        return new Sprite(ruta_carpeta + "/Enemigos/Goomba/Goomba_caminando_loop.gif", 32, 32);
     }
     public Sprite crearSpriteBuzzyBeetle() {
-        return new Sprite(ruta_carpeta + "/Enemigos/BuzzyBeetle/BuzzyBeetleLeftLoop.gif", 16, 16);
+        return new Sprite(ruta_carpeta + "/Enemigos/BuzzyBeetle/BuzzyBeetleLeftLoop.gif", 32, 32);
     }
     public Sprite crearSpriteKoopaTroopa(){
-        return new Sprite(ruta_carpeta + "/Enemigos/KoopaTroopa/KoopaCaminandoLeft.gif", 16, 16);
+        return new Sprite(ruta_carpeta + "/Enemigos/KoopaTroopa/KoopaCaminandoLeft.gif", 32, 32);
     }
     public Sprite crearSpriteLakitu() {
-        return new Sprite(ruta_carpeta + "/Enemigos/Lakitu_moviendose.png", 16, 16);
+        return new Sprite(ruta_carpeta + "/Enemigos/Lakitu_moviendose.png", 32, 32);
     }
     public Sprite crearSpriteSpiny() {
-        return new Sprite(ruta_carpeta + "/Enemigos/Spiny/SpinnyCaminandoLeftLoop.gif", 16, 16);
+        return new Sprite(ruta_carpeta + "/Enemigos/Spiny/SpinnyCaminandoLeftLoop.gif", 32, 32);
     }
     public Sprite crearSpriteBowser() {
-        return new Sprite(ruta_carpeta + "/Enemigos/Bowser/Bowser.png", 16, 16);
+        return new Sprite(ruta_carpeta + "/Enemigos/Bowser/Bowser.png", 32, 32);
     }
     public Sprite crearSpritePiranhaPlant() {
-        return new Sprite(ruta_carpeta + "/Enemigos/PiranhaPlant/PlantaLoop.gif", 16, 16);
+        return new Sprite(ruta_carpeta + "/Enemigos/PiranhaPlant/PlantaLoop.gif", 32, 32);
     }
     public Sprite crearSpriteMoneda() {
-        return new Sprite(ruta_carpeta + "/Pickupeables/Coin.png", 16, 16);
+        return new Sprite(ruta_carpeta + "/Pickupeables/Coin.png", 32, 28);
     }
     public Sprite crearSpriteChampinionVerde() {
-        return new Sprite(ruta_carpeta + "/Pickupeables/HongoVerde.png", 16, 16);
+        return new Sprite(ruta_carpeta + "/Pickupeables/HongoVerde.png", 32, 32);
     }
     public Sprite crearSpriteEstrella() {
-        return new Sprite(ruta_carpeta + "/Pickupeables/Estrella.png", 16, 16);
+        return new Sprite(ruta_carpeta + "/Pickupeables/Estrella.png", 32, 32);
     }
     public Sprite crearSpriteFlorDeFuego() {
-        return new Sprite(ruta_carpeta + "/Pickupeables/FlorDeFuego.png", 16, 16);
+        return new Sprite(ruta_carpeta + "/Pickupeables/FlorDeFuego.png", 32, 32);
     }
     public Sprite crearSpriteSuperChampinion() {
-        return new Sprite(ruta_carpeta + "/Pickupeables/Hongo.png", 16, 16);
+        return new Sprite(ruta_carpeta + "/Pickupeables/Hongo.png", 32, 32);
     }
     public Sprite crearSpriteBolaDeFuego() {
         return new Sprite(ruta_carpeta + "/", 16, 16);
@@ -58,13 +56,13 @@ public abstract class FabricaSprites {
     }
 
     public Sprite crearSpriteBloqueSolido() {
-        return new Sprite(ruta_carpeta + "/", 16, 16);
+        return new Sprite(ruta_carpeta + "/", 32, 32);
     }
     public Sprite crearSpriteTuberia() {
-        return new Sprite(ruta_carpeta + "/Bloques/Tuberia.png", 16, 16);
+        return new Sprite(ruta_carpeta + "/Bloques/Tuberia.png", 32, 32);
     }
     public Sprite crearSpriteBloquePregunta() {
-        return new Sprite(ruta_carpeta + "/", 16, 16);
+        return new Sprite(ruta_carpeta + "/Bloques/BloquePregunta.png", 32, 32);
     }
     public Sprite crearSpriteJugador() {
 
