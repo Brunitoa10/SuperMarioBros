19901f2ed4b389377505d0d47c93c39b2e002141
Francisco <franpiccio174@gmail.com>
2024-10-20 22:59:35 -0300

    Arreglo del sonido

--- src/Generador/GestorSonido/SonidoGenerico.java
+++ src/Generador/GestorSonido/SonidoGenerico.java
@@ -3,13 +3,14 @@ package Generador.GestorSonido;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import java.io.File;
 
 public class SonidoGenerico implements Sonido {
     private Clip clip;
 
     public SonidoGenerico(String rutaArchivo) {
         try {
-            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(getClass().getResource(rutaArchivo));
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(rutaArchivo));
             clip = AudioSystem.getClip();
             clip.open(audioInputStream);
         } catch (Exception e) {
