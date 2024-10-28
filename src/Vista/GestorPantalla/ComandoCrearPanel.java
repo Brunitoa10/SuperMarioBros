67b07d3d8a8c970837cc26e403ebd23cc0259156
Bruno A Parisi <bnoprs00@gmail.com>
2024-10-27 21:23:28 -0300

    Elimine paquete que no se usaba

--- src/Vista/GestorPantalla/ComandoCrearPanel.java
+++
@@ -1,9 +0,0 @@
-package Vista.GestorPantalla;
-
-import javax.swing.JPanel;
-
-import Vista.Controladores.ControladorVista;
-
-public interface ComandoCrearPanel {
-	public JPanel ejecutar(ControladorVista controlador);
-}
