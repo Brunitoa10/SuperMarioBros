565432f5bbe984097e5770c80e794fd7ac65e1c2
Dino123213 <dinoromagnoli14@gmail.com>
2024-11-05 01:22:06 -0300

    Ultimo push.Creo

--- src/Entidades/Power_Ups/ChampinionVerde.java
+++ src/Entidades/Power_Ups/ChampinionVerde.java
@@ -7,16 +7,14 @@ import Fabricas.Sprite;
 import java.util.List;
 
 public class ChampinionVerde extends PowerUp {
-    protected boolean elimine=false;
     public ChampinionVerde(int x, int y, Sprite sprite, List<PowerUp> listaPowerUpsNivel) {
         super(x, y, sprite, listaPowerUpsNivel);
     }
 
     @Override
     public int setEstadoMario(Jugador j) {
-        if(!elimine) {
+        if(!this.aEliminar) {
             j.sumarUnaVida(true);
-            elimine=true;
         }
         return ConstantesPuntaje.PUNTAJE_CHAMPINON_VERDE;
     }
